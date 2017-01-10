109061876506c70c45d34e67f884979a4d7bd43a
nesrine3ayadi <nesrine3ayadi@gmail.com>
2017-01-10 19:08:59 +0100

    Inscrire Controller

---
+++ app/Http/Controllers/InscrireController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InscrireController extends Controller
+{
+    //
+
+public function inscrire()
+{
+    return view('inscrire.index');
+
+
+}
+}
+
+
